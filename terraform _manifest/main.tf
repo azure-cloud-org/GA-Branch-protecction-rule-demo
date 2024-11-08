dd44aeb2e76a8511c9f72df5ea66b2a22e64b490
Vivek <dabhadev92@gmail.com>
2024-11-08 16:28:30 +0530

    Github branch protection policy-pr workflow-secret detection rulde demo

--- terraform _manifest/main.tf
+++ terraform _manifest/main.tf
@@ -12,3 +12,13 @@ resource "azurerm_virtual_network" "myvnet-branch-protection" {
   location            = azurerm_resource_group.myrg-branch-protection.location
   resource_group_name = azurerm_resource_group.myrg-branch-protection.name
 }
+
+##Test the Github secret detection rule
+resource "azurerm_mssql_server" "example-branch-protection" {
+  name                         = "example-sqlserver-branch-protection"
+  resource_group_name          = azurerm_resource_group.myrg-branch-protection.name
+  location                     = azurerm_resource_group.myrg-branch-protection.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
